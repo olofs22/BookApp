c57799d2309a94a5ff16acb3ea670718a4041da8
olofs22 <olof.business.svensson@gmail.com>
2026-09-18 13:13:08 +0200

    JWT token generation and TokenService, hashing salting through Identity (PBKDF2), everything talking with database AZure SQL for development, auth endpoints working, swagger test, password has requirments.

--- BookQuoteApp.Api/Controllers/AuthController.cs
+++ BookQuoteApp.Api/Controllers/AuthController.cs
@@ -1,6 +1,52 @@
-﻿namespace BookQuoteApp.Api.Controllers
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using BookQuoteApp.Api.Models;
+using BookQuoteApp.Api.DTOs;
+using BookQuoteApp.Api.Services;
+
+namespace BookQuoteApp.Api.Controllers
 {
-    public class AuthController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly TokenService _tokenService;
+        public AuthController(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            TokenService tokenService)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _tokenService = tokenService;
+        }
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterDTO rDto)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = rDto.Email,
+                Email = rDto.Email,
+                Name = rDto.Name,
+            };
+            var result = await _userManager.CreateAsync(user, rDto.Password);
+            if (!result.Succeeded)
+                return BadRequest(result.Errors);
+            return Ok(new { message = "User registered successfully" });
+        }
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginDTO lDto)
+        {
+            var user = await _userManager.FindByEmailAsync(lDto.Email);
+            if (user == null)
+                return Unauthorized(new {message = ("Invalid email or password")});
+            var passwordValid = await _userManager.CheckPasswordAsync(user, lDto.Password);
+            if (!passwordValid)
+                return Unauthorized(new { message = ("Invalid email or password") });
+            var token = _tokenService.GenerateToken(user);
+            return Ok(new { token = token });
+        }
     }
 }
--- BookQuoteApp.Api/Program.cs
+++ BookQuoteApp.Api/Program.cs
@@ -1,13 +1,25 @@
 using BookQuoteApp.Api.Data;
+using BookQuoteApp.Api.Services;
+using BookQuoteApp.Api.Models;
 using Microsoft.EntityFrameworkCore;
-
+using Microsoft.AspNetCore.Identity;
+using Swashbuckle.AspNetCore;
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddScoped<TokenService>();
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+{
+    options.Password.RequireDigit = true;
+    options.Password.RequiredLength = 8;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireNonAlphanumeric = false;
+})
+.AddEntityFrameworkStores<ApplicationDbContext>()
+.AddDefaultTokenProviders();
+
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -17,7 +29,9 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    //app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
