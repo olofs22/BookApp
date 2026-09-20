50f0edd685a8e528b4b5b16262e1935caf3a6a3c
olofs22 <olof.business.svensson@gmail.com>
2026-09-21 00:40:08 +0200

    Started working on controllers...

--- BookQuoteApp.Api/Controllers/BooksController.cs
+++ BookQuoteApp.Api/Controllers/BooksController.cs
@@ -1,6 +1,57 @@
-﻿namespace BookQuoteApp.Api.Controllers
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BookQuoteApp.Api.Data;
+using BookQuoteApp.Api.Models;
+using BookQuoteApp.Api.DTOs;
+
+
+namespace BookQuoteApp.Api.Controllers
 {
-    public class BooksController
+    [Authorize]
+    [ApiController]
+    [Route("api/controller")]
+    public class BooksController : ControllerBase
     {
+        private readonly ApplicationDbContext _dbContext;
+
+        public BooksController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var books = await _dbContext.Books.Include(b => b.Author).Include(b => b.Publisher).ToListAsync();
+            var bookDtos = books.Select(b => new BookDTO
+            {
+                Id = b.Id,
+                Title = b.Title,
+                AuthorName = b.Author.Name,
+                PublisherName = b.Publisher != null ? b.Publisher.Name : null,
+                PublishDate = b.PublishDate
+            }).ToList();
+
+            return Ok(bookDtos);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var book = await _dbContext.Books.Include(b => b.Author).Include(b => b.Publisher).FirstOrDefaultAsync(b => b.Id == id);
+
+            if (book == null)
+                return NotFound();
+
+            var bookDTO = new BookDTO
+            {
+                Id = book.Id,
+                Title = book.Title,
+                AuthorName = book.Author.Name,
+                PublisherName = book.Publisher != null ? book.Publisher.Name : null,
+                PublishDate = book.PublishDate
+            };
+
+            return Ok(bookDTO);
+        }
     }
 }
--- BookQuoteApp.Api/Program.cs
+++ BookQuoteApp.Api/Program.cs
@@ -14,7 +14,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
 {
