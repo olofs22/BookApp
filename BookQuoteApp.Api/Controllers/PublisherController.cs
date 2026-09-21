9dcca0e46a2a74cda20e7adbe43086c46c3b17a4
olofs22 <olof.business.svensson@gmail.com>
2026-09-21 13:36:42 +0200

    backend almost finished, need validations. Added publishercontroller and authorcontroller.

---
+++ BookQuoteApp.Api/Controllers/PublisherController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BookQuoteApp.Api.Data;
+using BookQuoteApp.Api.DTOs;
+
+namespace BookQuoteApp.Api.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class PublisherController : ControllerBase
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public PublisherController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var publishers = await _dbContext.Publisher.ToListAsync();
+            var publisherDto = publishers.Select(p => new PublisherDTO
+            {
+                Id = p.Id,
+                Name = p.Name
+            });
+            return Ok(publisherDto);
+        }
+    }
+}
